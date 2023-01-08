84a76259eb0484aaec7dac18c800ff57a72b57d6
Wasin Mekkit <w.mekkit@primal.co.th>
2023-01-08 15:57:21 +0700

    section: Single cask bottles

--- assets/js/partial/homepage.js
+++ assets/js/partial/homepage.js
@@ -24,3 +24,22 @@ var HeroBannerSwiper = new Swiper(".swiper__herobanner", {
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  var caskBottles = document.querySelector('.blk__caskbottles');
+  window.addEventListener('scroll', function() {
+    var caskBottlesTop = caskBottles.offsetTop;
+    var caskBottlesHeight = caskBottles.offsetHeight;
+    var windowTop = window.scrollY;
+    if (caskBottlesTop + caskBottlesHeight >= windowTop && caskBottlesTop <= windowTop + window.innerHeight) {
+      var caskBottlesImages = caskBottles.querySelectorAll('img');
+      for (var i = 0; i < caskBottlesImages.length; i++) {
+        caskBottlesImages[0].classList.add('animate__animated', 'animate__fadeInLeft', 'animate__delay-0.5s');
+        caskBottlesImages[1].classList.add('animate__animated', 'animate__fadeInLeft', 'animate__delay-0.9s');
+        caskBottlesImages[2].classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-0.5s');
+        caskBottlesImages[3].classList.add('animate__animated', 'animate__fadeInRight', 'animate__delay-0.9s');
+        caskBottlesImages[4].classList.add('animate__animated', 'animate__fadeInRight', 'animate__delay-0.5s');
+      }
+    }
+  });
+});
+
