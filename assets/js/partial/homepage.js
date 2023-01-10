04fda3e8d1cd8310b853dfae086de9201f03b533
Wasin Mekkit <w.mekkit@primal.co.th>
2023-01-10 12:27:39 +0700

    init: membership

--- assets/js/partial/homepage.js
+++ assets/js/partial/homepage.js
@@ -24,6 +24,39 @@ var HeroBannerSwiper = new Swiper(".swiper__herobanner", {
 });
 
 
+var MembershipSwiper = new Swiper('.swiper__membership', {
+  slidesPerView: 4,
+  spaceBetween: 50,
+  draggable: true,
+  pagination: {
+    el: '.swiper-container',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var caskBottles = document.querySelector('.blk__caskbottles');
   window.addEventListener('scroll', function() {
