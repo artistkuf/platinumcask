0470f1476c20f514f97784830b0945a89a044363
Wasin Mekkit <w.mekkit@primal.co.th>
2023-02-03 10:30:26 +0700

    Update: how it works

--- assets/js/partial/shop.js
+++ assets/js/partial/shop.js
@@ -1,28 +1,46 @@
-document.querySelectorAll('.shop__link a').forEach(link => {
-   link.addEventListener('click', function (e) {
-     e.preventDefault();
-     const targetId = e.target.getAttribute('href');
-     const targetPosition = document.querySelector(targetId).offsetTop;
-     const startPosition = window.pageYOffset;
-     const distance = targetPosition - startPosition;
-     const duration = 1000;
-     let start = null;
-     
-     window.requestAnimationFrame(step);
-     
-     function step(timestamp) {
-       if (!start) start = timestamp;
-       const progress = timestamp - start;
-       window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-       if (progress < duration) window.requestAnimationFrame(step);
-     }
-     
-     function easeInOutCubic(t, b, c, d) {
-       t /= d / 2;
-       if (t < 1) return c / 2 * t * t * t + b;
-       t -= 2;
-       return c / 2 * (t * t * t + 2) + b;
-     }
-   });
- });
- 
+document.querySelectorAll(".shop__link a").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = e.target.getAttribute("href");
+    const targetPosition = document.querySelector(targetId).offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1000;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      window.scrollTo(
+        0,
+        easeInOutCubic(progress, startPosition, distance, duration)
+      );
+      if (progress < duration) window.requestAnimationFrame(step);
+    }
+
+    function easeInOutCubic(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t * t + b;
+      t -= 2;
+      return (c / 2) * (t * t * t + 2) + b;
+    }
+  });
+});
+
+var ProductSwiper = new Swiper(".swiper__product", {
+  pagination: {
+    el: ".swiper-container",
+    type: "bullets",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true,
+  grabCursor: true,
+  lazy: true
+});
